3b48c7784faef8eda8d2b3c09ef84c22937e39e2
CarlosShu <70170956+CarlosShu@users.noreply.github.com>
2020-09-04 17:40:59 -0400

    Activity 2: Added comments.

--- Activities/02_Draw_An_Alien/js/script.js
+++ Activities/02_Draw_An_Alien/js/script.js
@@ -1,13 +1,13 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Activity 2: Draw an Alien
+Carlos-Enrique Salazar Aguilar
 
-Here is a description of this template p5 project.
+Draws an alien on the canvas.
 **************************************************/
 
 // setup()
 //
-// Description of setup() goes here.
+// Draws an alien.
 function setup() {
     createCanvas(640, 480);
 
@@ -41,7 +41,7 @@ function setup() {
 
 // draw()
 //
-// Description of draw() goes here.
+// Does nothing.
 function draw() {
 
 }
